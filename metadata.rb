4cbd0d52aea4fd82f0b4921fc27e4b169fca785f
John Keiser <jkeiser@opscode.com>
2013-04-10 15:26:33 -0700

    Get rid of dependencies Private Chef does not have

--- metadata.rb
+++ metadata.rb
@@ -9,20 +9,20 @@ version          "0.2.0"
 recipe "opscode-bifrost", "Installs Erlang and rebar (for now)"
 recipe "database", "Creates the bifrost database, schema, and users"
 
-depends "opscode-postgresql", "~> 0.1.6"
+#depends "opscode-postgresql", "~> 0.1.6"
 
 # This is the version we currently have in prod / preprod
 #
 # It apparently does not play nicely with Ubuntu 12?
-depends "apt", "= 1.2.2"
+#depends "apt", "= 1.2.2"
 
 # This is one of the new cookbooks:
 # https://github.com/opscode-cookbooks/opscode-pedant
-depends "opscode-pedant", "~> 0.1.2"
+#depends "opscode-pedant", "~> 0.1.2"
 
 # These come from our infrastructure
-depends "erlang_binary", "~> 0.0.3"
-depends "runit", "0.13.0" # internal fork
-depends "perl"
-depends "git"
-depends "python"
+#depends "erlang_binary", "~> 0.0.3"
+#depends "runit", "0.13.0" # internal fork
+#depends "perl"
+#depends "git"
+#depends "python"
