ff37158b8f1415f13e8e783a5f61a46be7c0dcea
John Keiser <jkeiser@opscode.com>
2013-04-10 10:34:33 -0700

    Make service recipe work with private chef config

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,51 +1,89 @@
-link "link_etc_dir_into_srv_dir" do
-  to node['oc_bifrost']['etc_dir']
-  target_file "#{node['oc_bifrost']['srv_dir']}/etc"
-  owner "opscode"
-  group "opscode"
+#
+# Set up private chef vs. hosted chef config
+#
+if node['private_chef']
+  bifrost_config = node['private_chef']['oc_bifrost']
+  service_owner = node['private_chef']['user']['username']
+  service_group = node['private_chef']['user']['username']
+  estatsd_host = node['private_chef']['estatsd']['vip']
+  estatsd_port = node['private_chef']['estatsd']['port']
+  db_host = node['private_chef']['postgresql']['vip']
+  db_port = 5432
+else
+  bifrost_config = node['oc_bifrost']
+  service_owner = 'opscode'
+  service_group = 'opscode'
+
+  # TODO: need to get this from search?
+  if node['stats_hero'] && node['stats_hero']['estatsd_host']
+    estatsd_host = node['stats_hero']['estatsd_host']
+  else
+    estatsd_host = data_bag_item("vips", node[:app_environment])['estatsd_host']
+  end
+  estatsd_port = node['stats_hero']['estatsd_port']
+
+  db_host = node['oc_bifrost']['database']['host'] || search(:node, "role:authz-pgsql")[0].ipaddress
+  db_port = node['oc_bifrost']['database']['port']
 end
 
-directory node['oc_bifrost']['bin_dir'] do
-  owner "opscode"
-  group "opscode"
+# TODO remove srv_dir and log_dir options (private chef uses dir and log_directory)
+service_dir = bifrost_config['dir'] || bifrost_config['srv_dir']
+log_dir = bifrost_config['log_directory'] || bifrost_config['log_dir']
+
+#
+# Ensure service directories exist
+#
+[ service_dir, "#{service_dir}/etc", log_dir, "#{log_dir}/sasl" ].each do |dir_name|
+  directory dir_name do
+    owner service_owner
+    group service_group
+    mode '0700'
+    recursive true
+  end
+end
+
+link "#{service_dir}/log" do
+  to log_dir
+  owner service_owner
+  group service_group
+end
+
+directory "#{service_dir}/bin" do
+  owner service_owner
+  group service_group
   mode "0755"
 end
 
-if node['stats_hero'] && node['stats_hero']['estatsd_host']
-  estatsd_host = node['stats_hero']['estatsd_host']
-else
-  estatsd_host = data_bag_item("vips", node[:app_environment])['estatsd_host']
-end
-
+#
+# Set up scripts and configuration
+#
 config_variables = {
-  :ip        => node['oc_bifrost']['host'],
-  :port      => node['oc_bifrost']['port'],
-  :db_host   => node['oc_bifrost']['database']['host'] || search(:node, "role:authz-pgsql")[0].ipaddress,
-  :db_port   => node['oc_bifrost']['database']['port'],
-  :db_name   => node['oc_bifrost']['database']['name'],
-  :db_user   => node['oc_bifrost']['database']['users']['owner']['name'],
-  :db_pass   => node['oc_bifrost']['database']['users']['owner']['password'],
-  :pool_size => node['oc_bifrost']['database']['connection_pool_size'],
-  :max_pool_size => node['oc_bifrost']['database']['max_connection_pool_size'],
-  :log_dir   => node['oc_bifrost']['log_dir'],
-  :udp_socket_pool_size => node['oc_bifrost']['stats_hero_udp_socket_pool_size'],
-
-  # TODO: need to get this from search?
+  :ip        => bifrost_config['host'],
+  :port      => bifrost_config['port'],
+  :db_host   => db_host,
+  :db_port   => db_port,
+  :db_name   => bifrost_config['database']['name'],
+  :db_user   => bifrost_config['database']['users']['owner']['name'],
+  :db_pass   => bifrost_config['database']['users']['owner']['password'],
+  :pool_size => bifrost_config['database']['connection_pool_size'],
+  :max_pool_size => bifrost_config['database']['max_connection_pool_size'],
+  :log_dir   => log_dir,
+  :udp_socket_pool_size => bifrost_config['stats_hero_udp_socket_pool_size'],
   :estatsd_host => estatsd_host,
-  :estatsd_port => node['stats_hero']['estatsd_port']
+  :estatsd_port => estatsd_port
 }
 
-template "#{node['oc_bifrost']['etc_dir']}/sys.config" do
-  owner "opscode"
-  group "opscode"
+template "#{service_dir}/etc/sys.config" do
+  owner service_owner
+  group service_group
   mode 0644
   variables(config_variables)
   notifies :restart, "service[oc_bifrost]", :delayed
 end
 
-template "#{node['oc_bifrost']['etc_dir']}/vm.args" do
-  owner "opscode"
-  group "opscode"
+template "#{service_dir}/etc/vm.args" do
+  owner service_owner
+  group service_group
   mode 0644
   notifies :restart, "service[oc_bifrost]", :delayed
 end
@@ -53,47 +91,72 @@ end
 # This is the script that will actually run the application.  It is
 # enhanced from the standard Erlang release boot script in that it has
 # support for running under runit.
-template "#{node['oc_bifrost']['bin_dir']}/oc_bifrost" do
+template "#{service_dir}/bin/oc_bifrost" do
   source "run_script.sh.erb"
-  owner "opscode"
-  group "opscode"
+  owner service_owner
+  group service_group
   mode 0755
-  variables(:log_dir => node['oc_bifrost']['log_dir'])
+  variables(config_variables)
   notifies :restart, "service[oc_bifrost]", :delayed
 end
 
 # These are some stock scripts that the boot script needs to call.
-["nodetool", "erl"].each do |file|
-  cookbook_file "#{node['oc_bifrost']['bin_dir']}/#{file}" do
-    owner "opscode"
-    group "opscode"
-    mode 0755
+cookbook_file "#{service_dir}/bin/nodetool" do
+  owner service_owner
+  group service_group
+  mode 0755
+end
+
+cookbook_file "#{service_dir}/bin/erl" do
+  owner service_owner
+  group service_group
+  mode 0755
+end
+
+if !node['private_chef']
+  # Drop off an rsyslog configuration
+  template "/etc/rsyslog.d/30-oc_bifrost.conf" do
+    source "erlang_app_rsyslog.conf.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(:log_file_path => "/var/log/oc_bifrost.log")
+    notifies :restart, "service[rsyslog]"
   end
 end
 
-# Drop off an rsyslog configuration
-template "/etc/rsyslog.d/30-oc_bifrost.conf" do
-  source "erlang_app_rsyslog.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  variables(:log_file_path => "/var/log/oc_bifrost.log")
-  notifies :restart, "service[rsyslog]"
-end
+#
+# Set up the actual service
+#
 
 runit_service 'oc_bifrost' do
   template_name "erlang_app" # Our common template
+  if node['private_chef']
+    down node['private_chef']['oc_bifrost']['ha']
+  end
   options({
-            :srv_dir => node['oc_bifrost']['srv_dir'],
+            :srv_dir => service_dir,
             :bin_name => 'oc_bifrost',
 
             # See http://smarden.org/runit/chpst.8.html
 
             # These are for the run script
-            :run_setuidgid => "opscode",
-            :run_envuidgid => "opscode",
+            :run_setuidgid => service_owner,
+            :run_envuidgid => service_group,
 
             # This is for the log-run script
             :log_setuidgid => "nobody"
           })
+end
+
+if node['private_chef']
+  # Bootstrap me
+  if node['private_chef']['bootstrap']['enable']
+    execute "/opt/opscode/bin/private-chef-ctl start oc_bifrost" do
+      retries 20
+    end
+  end
+
+  # Everybody else is doing it ...
+  add_nagios_hostgroup("oc_bifrost")
 end
