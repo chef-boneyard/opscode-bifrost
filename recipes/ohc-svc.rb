e0d0be0725021e6ef3f65507331e7af86f9a4075
langloisjp <jpl@opscode.com>
2013-06-03 14:49:04 -0700

    Can't use node[...] = ...
    Using node.default instead.

--- recipes/ohc-svc.rb
+++ recipes/ohc-svc.rb
@@ -6,14 +6,14 @@ app = 'oc_bifrost'
 vips = data_bag_item("vips", node[:app_environment])
 
 # estatsd host is 1) node override or 2) VIP
-node[app]['estatsd_host'] = if node['stats_hero'] && node['stats_hero']['estatsd_host']
+node.default[app]['estatsd_host'] = if node['stats_hero'] && node['stats_hero']['estatsd_host']
    node['stats_hero']['estatsd_host']
 else
   vips['estatsd_host']
 end
 
 # DB host is 1) node override or 2) VIP or 3) role query
-node[app]['database']['host'] = if node[app]['database']['host']
+node.default[app]['database']['host'] = if node[app]['database']['host']
   Chef::Log.info("Using node attribute for #{app} DB host")
   node[app]['database']['host']
 elsif vips["bifrost_pgsql_ip"]
@@ -26,11 +26,11 @@ end
 
 # Superuser ID is in the environments data bag
 env = data_bag_item("environments", node[:app_environment])
-node[app]['superuser_id'] = env['opscode-authz-superuser-id']
+node.default[app]['superuser_id'] = env['opscode-authz-superuser-id']
 
 # DB passwords are in the secrets data bag.
 secrets = data_bag_item("secrets", node[:app_environment])
-node[app]['database']['users']['owner']['password'] = secrets[app]['db_rw_password']
-node[app]['database']['users']['read_only']['password'] = secrets[app]['db_ro_password']
+node.default[app]['database']['users']['owner']['password'] = secrets[app]['db_rw_password']
+node.default[app]['database']['users']['read_only']['password'] = secrets[app]['db_ro_password']
 
 include_recipe "opscode-bifrost::api_server"
