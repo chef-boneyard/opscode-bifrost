b929065bbeb65bbbf96176a89fbfbfea2cf290d9
langloisjp <jpl@opscode.com>
2013-06-18 16:45:31 -0700

    Wrap otp_build git definitions into its own method for readability.

--- providers/otp_build.rb
+++ providers/otp_build.rb
@@ -12,29 +12,7 @@ action :build do
     # Source is a git reference. Sync git repo.
     @run_context.include_recipe "git"
     src_dir = "#{new_resource.src_root_dir}/#{new_resource.name}"
-
-    if new_resource.force_clean_src
-      Chef::Log.info("*** Force cleanup of #{src_dir}")
-      execute "cleanup_#{new_resource.name}_src" do
-        command "rm -Rf #{src_dir}"
-      end
-    end
-
-    directory src_dir do
-      owner new_resource.owner
-      group new_resource.group
-      mode new_resource.dir_mode
-      recursive true
-    end
-
-    git "#{new_resource.name}_source" do
-      destination src_dir
-      repository new_resource.source # "git@github.com:opscode/#{app_name}.git"
-      revision new_resource.revision
-      user new_resource.owner
-      group new_resource.group
-      notifies :create, "ruby_block[cleanbuild_#{new_resource.name}]", :immediately
-    end
+    git_source_repo src_dir
   else
     # source is a local directory
     src_dir = new_resource.source
@@ -101,5 +79,30 @@ action :build do
     command "tar czf #{new_resource.tarball} #{new_resource.name}"
     cwd "#{src_dir}/rel"
     action :nothing
+  end
+end
+
+def git_source_repo(src_dir)
+  if new_resource.force_clean_src
+    Chef::Log.info("*** Force cleanup of #{src_dir}")
+    execute "cleanup_#{new_resource.name}_src" do
+      command "rm -Rf #{src_dir}"
+    end
+  end
+
+  directory src_dir do
+    owner new_resource.owner
+    group new_resource.group
+    mode new_resource.dir_mode
+    recursive true
+  end
+
+  git "#{new_resource.name}_source" do
+    destination src_dir
+    repository new_resource.source # "git@github.com:opscode/#{app_name}.git"
+    revision new_resource.revision
+    user new_resource.owner
+    group new_resource.group
+    notifies :create, "ruby_block[cleanbuild_#{new_resource.name}]", :immediately
   end
 end
