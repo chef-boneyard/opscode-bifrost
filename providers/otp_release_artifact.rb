52ed3d8dcc557b0c346ab2ffa8fdeecb3ca37d03
langloisjp <jpl@opscode.com>
2013-06-24 16:05:16 -0700

    Reserve LWRP defaults from node to exceptional cases only.
    The otp_release_artifact does not fall into that category.

--- providers/otp_release_artifact.rb
+++ providers/otp_release_artifact.rb
@@ -1,58 +1,34 @@
 # OTP release artifact LWRP provider. Builds or retrieves a release artifact.
 
 action :create do
-  opts = options(new_resource)
-
-  if opts[:development_mode]
+  if new_resource.development_mode
     # In dev we build from source. Source can be a local
     # directory, e.g. "/vagrant", or a git repo.
     # Generates a tar ball appname-revision.tgz and puts
     # it in dest_dir.
-    Chef::Log.info("*** In development mode, build #{opts[:tarball]} from #{opts[:source]}.")
-    opscode_bifrost_otp_build opts[:name] do
+    Chef::Log.info("*** In development mode, build #{new_resource.tarball} from #{new_resource.source}.")
+    opscode_bifrost_otp_build new_resource.name do
       action :build
-      tarball opts[:tarball]
-      source opts[:source]
-      revision opts[:revision]
-      force_clean_src opts[:force_clean_src]
-      owner opts[:owner]
-      group opts[:group]
-      src_root_dir opts[:src_root_dir]
+      tarball new_resource.tarball
+      source new_resource.source
+      revision new_resource.revision
+      force_clean_src new_resource.force_clean_src
+      owner new_resource.owner
+      group new_resource.group
+      src_root_dir new_resource.src_root_dir
     end
   else
     # Otherwise we deploy from artifact.
-    Chef::Log.info("*** Getting #{opts[:name]}-#{opts[:revision]} OTP release artifact from S3.")
-    opscode_bifrost_s3_artifact opts[:name] do
+    Chef::Log.info("*** Getting #{new_resource.name}-#{new_resource.revision} OTP release artifact from S3.")
+    opscode_bifrost_s3_artifact new_resource.name do
       action :sync
-      revision opts[:revision]
-      tarball opts[:tarball]
-      owner opts[:owner]
-      group opts[:group]
-      aws_bucket opts[:aws_bucket]
-      aws_access_key_id opts[:aws_access_key_id]
-      aws_secret_access_key opts[:aws_secret_access_key]
+      revision new_resource.revision
+      tarball new_resource.tarball
+      owner new_resource.owner
+      group new_resource.group
+      aws_bucket new_resource.aws_bucket
+      aws_access_key_id new_resource.aws_access_key_id
+      aws_secret_access_key new_resource.aws_secret_access_key
     end
   end
-end
-
-def options(r)
-    service = r.name
-    opts = r.to_hash
-
-    defaults = {
-        :source => node[service]['source'],
-        :development_mode => node[service]['development_mode'],
-        :owner => node[service]['owner'],
-        :group => node[service]['group'],
-        :revision => node[service]['revision'],
-        :aws_bucket => node[service]['aws_bucket'],
-        :aws_access_key_id => node[service]['aws_access_key_id'],
-        :aws_secret_access_key => node[service]['aws_secret_access_key']
-    }
-
-    defaults.each do |k, v|
-        opts[k] = v unless opts[k]
-    end
-
-    opts
 end
