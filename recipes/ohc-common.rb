b470933f3de12dc45667cd3dde4990f2a01b4ae3
langloisjp <jpl@opscode.com>
2013-07-02 11:26:29 -0700

    Use node.chef_environment.

--- recipes/ohc-common.rb
+++ recipes/ohc-common.rb
@@ -4,6 +4,6 @@ app = 'oc_bifrost'
 #
 # In OHC we get the revision to deploy from the env data bag.
 #
-env = data_bag_item("environments", node[:app_environment])
-bifrost_env = env[app] || (raise "missing #{app} key in #{node[:app_environment]} env data bag item")
+env = data_bag_item("environments", node.chef_environment)
+bifrost_env = env[app] || (raise "missing #{app} key in #{node.chef_environment} env data bag item")
 node.default[app]['revision'] = bifrost_env['revision'] || (raise "missing revision in oc_erchef env data")
