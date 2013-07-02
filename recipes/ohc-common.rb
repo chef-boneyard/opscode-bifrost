632c56e1b0384ad8a8e69377bf95cdb668f3f384
langloisjp <jpl@opscode.com>
2013-07-02 14:32:56 -0700

    Also get 'build-revision' from the data bag in OHC.
    'revision' can be a branch or commit id. build-revision must
    be the x.y.z revision of the app and is used for schema
    migrations.

--- recipes/ohc-common.rb
+++ recipes/ohc-common.rb
@@ -6,4 +6,5 @@ app = 'oc_bifrost'
 #
 env = data_bag_item("environments", node.chef_environment)
 bifrost_env = env[app] || (raise "missing #{app} key in #{node.chef_environment} env data bag item")
-node.default[app]['revision'] = bifrost_env['revision'] || (raise "missing revision in oc_erchef env data")
+node.default[app]['revision'] = bifrost_env['revision'] || (raise "missing revision in #{app} env data")
+node.default[app]['build-revision'] = bifrost_env['build-revision'] || (raise "missing build-revision in #{app} env data")
